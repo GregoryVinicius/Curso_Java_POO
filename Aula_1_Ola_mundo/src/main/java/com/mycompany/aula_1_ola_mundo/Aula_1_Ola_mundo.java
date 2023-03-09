0bdc4bd9abac7cb394929472723c05609d8b1562
GregoryVinicius <gregoryvinciuscla@gmail.com>
2023-03-09 15:57:24 -0300

    Curso POO

--- Aula_1_Ola_mundo/src/main/java/com/mycompany/aula_1_ola_mundo/Aula_1_Ola_mundo.java
+++ Aula_1_Ola_mundo/src/main/java/com/mycompany/aula_1_ola_mundo/Aula_1_Ola_mundo.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.aula_1_ola_mundo;
 
-/**
- *
- * @author Pichau
- */
 public class Aula_1_Ola_mundo {
 
     public static void main(String[] args) {
